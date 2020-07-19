fbf821a9f50a002c8546f26ee360a425dd2240b4
winterprogram <chakladar.sandeep4@gmail.com>
2020-07-19 20:29:55 +0530

    adding aws link

--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -5,6 +5,6 @@ const Color kErrorRed = Colors.redAccent;
 const Color kDarkGray = Color(0xFFA3A3A3);
 const Color kLightGray = Color(0xFFF1F0F5);
 // const kUrl = 'http://192.168.43.65:3000';
-const kUrl = 'http://13.234.122.21/';
+const kUrl = 'http://13.234.122.21';
 //const kUrl = 'http://10.0.2.2:3000';
 //{"error":true,"status":404,"message":"error blank data while updating payments","data":null}
